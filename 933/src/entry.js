be06211a52923ec5764734bb19a0cdb4cecda4a2
David Nagli <davidnagli@gmail.com>
2018-03-19 13:54:10 -0400

    933 alternative reproduction (diff content, same names)

---
+++ 933/src/entry.js
@@ -0,0 +1,5 @@
+import a from './folder1/subfolder/index.js'
+import b from './folder2/subfolder/index.js'
+
+console.log('a:',a());
+console.log('b:',b());
---
+++ 933/src/folder1/subfolder/index.js
@@ -0,0 +1,5 @@
+import number from '../something'
+
+export default function () {
+    return "hello from folder " + number;
+}
